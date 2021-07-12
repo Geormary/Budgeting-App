16913e2e16e4ed675f1c627abef313e6f5a871ad
Geormary Sanchez <geormarysanchez.gs@gmail.com>
2021-07-12 19:57:12 -0400

    initialized server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require("./app.js");
 const dotenv = require("dotenv");
 // Configuration
 dotenv.config();
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 5555;
 // Listen
 app.listen(PORT, () => {
   console.log(`💎 express is listening on port ${PORT}...`);
