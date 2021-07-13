a2af42abe55fa09d2fe43f738f9176342a44e067
Geormary Sanchez <geormarysanchez.gs@gmail.com>
2021-07-12 21:40:33 -0400

    created show route

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -2,8 +2,20 @@ const express = require("express");
 const transactions = express.Router();
 const transactionsArray = require("../models/transactions");
 
+//INDEX
 transactions.get("/", (req, res) => {
   res.status(200).json(transactionsArray)
 });
+//SHOW
+transactions.get("/:id",(req,res)=>{
+  const {id} = req.params
+  if(transactionsArray[id]){
+    res.json(200).json(transactionsArray[id])
+  } else {
+    res.redirect("/404");
+  }
+})
+
+
 
 module.exports = transactions;
