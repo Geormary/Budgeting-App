f6b9c01bac2acba5e084193337df4c2ee24016c4
Geormary Sanchez <geormarysanchez.gs@gmail.com>
2021-07-12 22:28:31 -0400

    created delete route

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -10,9 +10,26 @@ transactions.get("/", (req, res) => {
 transactions.get("/:id",(req,res)=>{
   const {id} = req.params
   if(transactionsArray[id]){
-    res.json(200).json(transactionsArray[id])
+    res.status(200).json(transactionsArray[id])
   } else {
     res.redirect("/404");
+  }
+})
+//CREATE
+transactions.post("/", (req,res)=>{
+  const {body} = req.body
+  transactionsArray.push(body)
+  res.json(transactionsArray[transactionsArray.length-1])
+})
+
+//DELETE
+transactions.delete("/:id",(req,res)=>{
+  const {id} = req.params
+  const deletedTransaction = transactionsArray.splice(id, 1);
+  if(transactionsArray[id]){
+    res.status(200).json(deletedTransaction);
+  } else {
+    res.redirect("/404")
   }
 })
 
