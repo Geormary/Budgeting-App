fa62367777a66b41fd8b3a878d82eb280c71e570
Geormary Sanchez <geormarysanchez.gs@gmail.com>
2021-07-15 15:45:30 -0400

    working back end

--- models/transactions.js
+++ models/transactions.js
@@ -13,5 +13,20 @@ module.exports = [
     date: "April 4",
     category: "Shopping",
     amount: "400"
+  },
+  {
+    date: "April 8",
+    category: "Income",
+    amount: "1000"
+  },
+  {
+    date: "April 10",
+    category: "Entertainment",
+    amount: "200"
+  },
+  {
+    date: "April 12",
+    category: "Shopping",
+    amount: "400"
   }
 ];
