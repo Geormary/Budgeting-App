755f36a2124b4caf9203c8fea9b4e6015cddca56
Geormary Sanchez <geormarysanchez.gs@gmail.com>
2021-07-14 20:28:03 -0400

    manually updated transaction array

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -7,38 +7,41 @@ transactions.get("/", (req, res) => {
   res.status(200).json(transactionsArray)
 });
 //SHOW
-transactions.get("/:idx",(req,res)=>{
-  const {idx} = req.params
-  if(transactionsArray[id]){
-    res.status(200).json(transactionsArray[idx])
+transactions.get("/:index",(req,res)=>{
+  const {index} = req.params
+  console.log(index)
+  if(transactionsArray[index]){
+    console.log(index)
+    res.status(200).json(transactionsArray[index])
   } else {
+    console.log(index)
     res.redirect("/404");
   }
 })
-//CREATE
-transactions.post("/", (req,res)=>{
-  const {body} = req.body
-  transactionsArray.push(body)
-  res.json(transactionsArray[transactionsArray.length-1])
-})
+// //CREATE
+// transactions.post("/", (req,res)=>{
+//   const {body} = req.body
+//   transactionsArray.push(body)
+//   res.json(transactionsArray[transactionsArray.length-1])
+// })
 
-//DELETE
-transactions.delete("/:idx",(req,res)=>{
-  const {id} = req.params
-  const deletedTransaction = transactionsArray.splice(idx, 1);
-  if(transactionsArray[idx]){
-    res.status(200).json(deletedTransaction);
-  } else {
-    res.redirect("/404")
-  }
-})
+// //DELETE
+// transactions.delete("/:idx",(req,res)=>{
+//   const {id} = req.params
+//   const deletedTransaction = transactionsArray.splice(idx, 1);
+//   if(transactionsArray[idx]){
+//     res.status(200).json(deletedTransaction);
+//   } else {
+//     res.redirect("/404")
+//   }
+// })
 
-//UPDATE
-transactions.put("/:idx", (req, res) => {
-  const {idx} = req.params
-  transactionsArray[idx] = req.body;
-  res.status(200).json(transactionsArray[idx]);
-});
+// //UPDATE
+// transactions.put("/:idx", (req, res) => {
+//   const {idx} = req.params
+//   transactionsArray[idx] = req.body;
+//   res.status(200).json(transactionsArray[idx]);
+// });
 
 
 
--- models/transactions.js
+++ models/transactions.js
@@ -4,4 +4,14 @@ module.exports = [
     category: "Income",
     amount: "1000",
   },
+  {
+    date: "April 2",
+    category: "Bill",
+    amount: "200",
+  },
+  {
+    date: "April 4",
+    category: "Shopping",
+    amount: "400",
+  },
 ];
